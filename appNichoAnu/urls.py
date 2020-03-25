c167af8c0dfd963baeac4eb9c17183f25eeb16a1
Duke Togo <golgo8128@yahoo.co.jp>
2020-03-25 14:14:05 +0900

    Massive copy and import

---
+++ appNichoAnu/urls.py
@@ -0,0 +1,35 @@
+from django.conf.urls import include, url
+
+from . import views, views_node_info1_4, views_edge_info1_2,  \
+    views_add_node1_1, views_add_edge1_1, \
+    views_del_node1_1, views_del_edge1_1
+
+urlpatterns = [
+
+    url(r'^node_info/node_vis_id=(?P<node_vis_id>[^@\s]+)$',
+        views_node_info1_4.node_vis_id_info, name="node_vis_id_info"),   
+    url(r'^node_info/node_vis_id=(?P<node_vis_id>\S+)@hwidth=(?P<hwidth>\d+)_hheight=(?P<hheight>\d+)_offx=(?P<offx>-?\d+)_offy=(?P<offy>-?\d+)$',
+        views_node_info1_4.node_vis_id_info, name="node_vis_id_info"),  
+    url(r'^edge_info/(?P<edge_id>\S+)$',
+        views_edge_info1_2.edge_info, name="edge_info"),
+    url(r'^del_node/(?P<node_vis_id>\S+)$',
+        views_del_node1_1.del_node, name="del_node"),
+    url(r'^del_edge/(?P<edge_id>\S+)$',
+        views_del_edge1_1.del_edge, name="del_edge"),
+    url(r'^add_node$',
+        views_add_node1_1.add_node, name="add_node"),
+    url(r'^add_edge$',
+        views_add_edge1_1.add_edge, name="add_edge"),
+    url(r'^$', views.index, name="index"),               
+    url(r'^info_pub$', views.pub_db_tools, name="pub_db_tools"),  
+    url(r'^survey$', views.survey, name="survey"),  
+    url(r'^member$', views.member, name="member"),  
+    url(r'^material_src$', views.material_src, name="material_src"),      
+    url(r'^history$', views.history, name="history"),  
+    url(r'^work_record$', views.work_record, name="work_record"), 
+    url(r'^blog$', views.blog, name="blog"),  
+
+#     url(r'^node_info/(?P<id>\S+)$',
+#         views.node_info, name="node_info"),     
+    
+]
---
+++ appNichoAnu/views.py
@@ -0,0 +1,107 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+import os
+from datetime import datetime, timedelta
+import dateutil.parser
+
+import getpass
+
+import matplotlib
+# Force matplotlib to not use any Xwindows backend.
+matplotlib.use('Agg')
+
+from django.conf import settings
+from django.template import RequestContext, loader
+from django.utils import timezone
+from django.http import HttpResponse, HttpResponseRedirect #, JsonResponse
+from django.core.urlresolvers import reverse
+
+from ipware.ip import get_ip
+
+from .models import *
+from .modules.to_networkx1_6 import to_networkx
+
+from UCSD_IMM_Usefuls.Time.timezone1 import get_tzone
+
+from FileDirPath.mkdir_on_absent import mkdir_on_absent
+from FileDirPath.File_Path1 import rs_filepath_info
+
+from Usefuls.TimeStamp_HashFile1_1 import TimeStamp_HashFile
+
+client_ip_log_file = os.path.join(settings.UCSD_IMM_WORKDIR,
+                                  "Media", "appNichoAnu", "Log",
+                                  "client_ip_log1.pkl")
+
+def index(request):
+
+    timezone.activate(get_tzone(request.user))  
+
+    ipaddr = get_ip(request)
+    if ipaddr:        
+        mkdir_on_absent(rs_filepath_info(client_ip_log_file)[ "foldername" ])
+        ip_log = TimeStamp_HashFile(client_ip_log_file)
+        ip_log.stamp(ipaddr)
+
+    contxt = RequestContext(request, { "NichoNode" : NichoNode,
+                                       "NichoEdge" : NichoEdge,
+                                       "settings"  : settings,
+                                       "apache_user" : getpass.getuser(),
+        "acount_minus_wk3": len(ip_log.get_keys_timerange(timedelta(days = 21), timedelta(days = 14))),
+        "acount_minus_wk2": len(ip_log.get_keys_timerange(timedelta(days = 14), timedelta(days =  7))),
+        "acount_minus_wk1": len(ip_log.get_keys_timerange(timedelta(days =  7), timedelta(days =  0))),
+         }) # {{ message|safe }}
+    templt = loader.get_template("appNichoAnu/map_Anurag1_3.html")
+    
+    return HttpResponse(templt.render(contxt)) 
+
+
+def pub_db_tools(request):
+
+    contxt = RequestContext(request, { })
+    templt = loader.get_template("appNichoAnu/pub_db_tools1.html")
+    
+    return HttpResponse(templt.render(contxt))     
+
+def survey(request):
+
+    contxt = RequestContext(request, { })
+    templt = loader.get_template("appNichoAnu/survey1.html")
+    
+    return HttpResponse(templt.render(contxt))    
+
+def member(request):
+
+    contxt = RequestContext(request, { })
+    templt = loader.get_template("appNichoAnu/nichoanu_member1.html")
+    
+    return HttpResponse(templt.render(contxt))     
+    
+def material_src(request):
+    
+    contxt = RequestContext(request, { })
+    templt = loader.get_template("appNichoAnu/material_src1.html")
+    
+    return HttpResponse(templt.render(contxt))    
+    
+def history(request):
+
+    contxt = RequestContext(request, { })
+    templt = loader.get_template("appNichoAnu/history_Anurag1.html")
+    
+    return HttpResponse(templt.render(contxt))  
+
+def work_record(request):
+
+    contxt = RequestContext(request, { })
+    templt = loader.get_template("appNichoAnu/work_record_Golgo1.html")
+    
+    return HttpResponse(templt.render(contxt)) 
+
+def blog(request):
+
+    contxt = RequestContext(request, { })
+    templt = loader.get_template("appNichoAnu/blog1.html")
+    
+    return HttpResponse(templt.render(contxt))   
